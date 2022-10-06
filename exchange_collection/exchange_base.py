0f169a5f6df8e84c8138614a3976b80e83548c76
bahadirkisbet <bahadirkisbet@gmail.com>
2022-10-06 10:35:48 +0300

    [refactor] fix and new methods

--- exchange_collection/exchange_base.py
+++ exchange_collection/exchange_base.py
@@ -1,14 +1,69 @@
+import configparser
+import logging
 from abc import abstractmethod, ABC
 from datetime import datetime
-import logging
-import configparser
-from CommonModels.DataModels import Interval
+from common_models.DataModels import Interval
+import json
+import websocket
+
 
 class ExchangeBase(ABC):
 
     def __init__(self, config: configparser.ConfigParser, logger: logging.Logger):
         self.config = config
         self.logger = logger
+        self.__data_callbacks__ = list()
+        self.__error_callbacks__ = list()
+        self.__websocket_dict__ = dict()
+
+    def register_callbacks(self, data_callbacks: list, error_callbacks=None) -> None:
+        for data_callback in data_callbacks:
+            self.__data_callbacks__.append(data_callback)
+        if error_callbacks is not None:
+            for error_callback in error_callbacks:
+                self.__error_callbacks__.append(error_callback)
+
+    def __create_websocket_connection__(self, address, port=None):
+        def on_message(ws: websocket.WebSocketApp, message):
+            msg = json.loads(message)
+            self.logger.info("Message has received: %s" % msg)
+            # publish to callbacks
+            for callback in self.__data_callbacks__:
+                callback(msg)
+
+        def on_error(ws: websocket.WebSocketApp, error):
+            self.logger.error(error)
+            for callback in self.__error_callbacks__:
+                callback(error)
+
+        def on_close(ws: websocket.WebSocketApp, close_status_code, close_msg):
+            self.logger.warning("websocket is closed", close_status_code, close_msg)
+
+        def on_open(ws: websocket.WebSocketApp):
+            self.logger.info("Websocket connection is done to %s" % ws.url)
+
+        url = "wss://" + address
+        if port is not None:
+            url += ":" + str(port)
+
+        socket = websocket.WebSocketApp(
+            url=url,
+            on_message=on_message,
+            on_error=on_error,
+            on_close=on_close,
+            on_open=on_open,
+        )
+        socket_name = f"SOCKET_{len(self.__websocket_dict__) + 1}"
+        self.__websocket_dict__[socket_name] = socket
+        socket.run_forever()
+        return socket_name
+
+    @abstractmethod
+    def fetch_product_list(self):
+        """
+        :return: Retrieves trade-able product list of the relevant exchange
+        """
+        pass
 
     @abstractmethod
     def fetch_candle(self, symbol: str, startDate: datetime, endDate: datetime, interval: str) -> list:
@@ -25,7 +80,7 @@ class ExchangeBase(ABC):
                     - Example: "1m", "5m", "1h", "1d"
         """
         pass
-    
+
     @abstractmethod
     def subscribe_to_websocket(self, symbol: str, interval: Interval) -> None:
         """
@@ -37,7 +92,7 @@ class ExchangeBase(ABC):
                     - Example: "1m", "5m", "1h", "1d"
         """
         pass
-    
+
     @abstractmethod
     def unsubscribe_from_websocket(self, symbol: str, interval: Interval) -> None:
         """
@@ -49,8 +104,7 @@ class ExchangeBase(ABC):
                     - Example: "1m", "5m", "1h", "1d"
         """
         pass
-        
-    
+
     def interval_to_granularity(self, interval: Interval) -> int:
         """
             Converts interval to the requested granularity of an exchange.
@@ -63,4 +117,3 @@ class ExchangeBase(ABC):
                         - "1d" -> 86400
         """
         pass
-    
--- exchange_collection/exchange_library/FTX.py
+++ exchange_collection/exchange_library/FTX.py
@@ -2,48 +2,49 @@ from datetime import timedelta, datetime
 from queue import Queue
 from threading import Thread
 from time import sleep
-import logging
-
 import requests
-from ExchangeCollection.ExchangeBase import *
+from exchange_collection.exchange_base import *
+
 
 class FTX(ExchangeBase):
     """
         FTX is a cryptocurrency exchange.
         - https://ftx.com/
     """
-
     name: str = "FTX"
     websocket_url: str = "wss://ftx.com/ws/v2"
     api_url: str = "https://ftx.com/api"
     api_endpoints: dict = {
-        "fetch_candle": "/markets/{symbol}/candles?granularity={granularity}&start={start}&end={end}"
+        "fetch_candle": "/markets/{symbol}/candles?granularity={granularity}&start={start}&end={end}",
+        "fetch_product_list": "/markets"
     }
 
     def __init__(self, config: configparser.ConfigParser, logger: logging.Logger) -> None:
         super().__init__(config, logger)
         self.limit = 1000
         self.throttle_seconds = 0.2
-        
 
-    def fetch_candle(self, symbol: str, startDate: datetime, endDate: datetime, interval: Interval) -> list:
+    def fetch_product_list(self):
+        pass
+
+    def fetch_candle(self, symbol: str, start_date: datetime, end_date: datetime, interval: Interval) -> list:
         interval_in_seconds = self.interval_to_granularity(interval)
         url = self.api_url + self.api_endpoints["fetch_candle"]
 
         params = []
-    
-        while startDate <= endDate:
-            startTime = int(startDate.timestamp())
-            endTime = int((startDate + timedelta(seconds=interval_in_seconds * (self.limit - 1))).timestamp())
 
-            params.append( 
-                url.format( 
-                    market_name=symbol, 
-                    resolution=interval_in_seconds, 
-                    start_time=startTime, 
-                    end_time=endTime)
-                )
-            startDate += timedelta(seconds=(interval_in_seconds * self.limit))
+        while start_date <= end_date:
+            start_time = int(start_date.timestamp())
+            end_time = int((start_date + timedelta(seconds=interval_in_seconds * (self.limit - 1))).timestamp())
+
+            params.append(
+                url.format(
+                    market_name=symbol,
+                    resolution=interval_in_seconds,
+                    start_time=start_time,
+                    end_time=end_time)
+            )
+            start_date += timedelta(seconds=(interval_in_seconds * self.limit))
 
         queue: Queue = Queue()
         tasks = list()
@@ -59,26 +60,30 @@ class FTX(ExchangeBase):
         result = list()
         while self.queue.empty() is False:
             data = self.queue.get()
-            if data.get("success", False): # if the request was successful, get the data
+            if data.get("success", False):  # if the request was successful, get the data
                 result.extend(data.get("result", []))
-        
+        return result
 
     def __get_candle__(self, url: str, queue: Queue) -> list:
         try:
             response = requests.get(url)
             queue.put(response.json())
         except Exception as e:
-            print(e)
+            self.logger.exception(e)
 
     def interval_to_granularity(self, interval: Interval) -> int:
         match interval:
-            case Interval.ONE_MINUTES: return 60
-            case Interval.FIVE_MINUTES: return 300
-            case Interval.FIFTEEN_MINUTES: return 900
-            case Interval.ONE_HOUR: return 3600
-            case Interval.ONE_DAY: return 86400
+            case Interval.ONE_MINUTES:
+                return 60
+            case Interval.FIVE_MINUTES:
+                return 300
+            case Interval.FIFTEEN_MINUTES:
+                return 900
+            case Interval.ONE_HOUR:
+                return 3600
+            case Interval.ONE_DAY:
+                return 86400
         return 0
-
 
     def subscribe_to_websocket(self, symbol: str, interval: Interval) -> None:
         pass
