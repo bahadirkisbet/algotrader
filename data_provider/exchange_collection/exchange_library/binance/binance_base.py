dfa32ae3e8e961d3645bed6dadd7df725065d653
bahadirkisbet <bahadirkisbet@gmail.com>
2022-12-10 21:33:44 +0300

    [feature] interval conversion from this project to binance

--- data_provider/exchange_collection/exchange_library/binance/binance_base.py
+++ data_provider/exchange_collection/exchange_library/binance/binance_base.py
@@ -1,15 +1,12 @@
 from data_provider.exchange_collection.exchange_base import *
-import requests
 from common_models.time_models import Interval
-from common_models.exchange_type import ExchangeType
 
 
-class BinanceBase(ExchangeBase):
+class BinanceBase(ExchangeBase, ABC):
     """
         Binance is a cryptocurrency exchange.
         - https://www.binance.com/
     """
-
     name: str = "BNB"
     websocket_url: str | None = None
     api_url: str | None = None
@@ -17,3 +14,18 @@ class BinanceBase(ExchangeBase):
 
     def __init__(self, config: configparser.ConfigParser, logger: logging.Logger):
         super().__init__(config, logger)
+
+    def interval_to_granularity(self, interval: Interval) -> object:
+        match interval:
+            case Interval.ONE_MINUTES:
+                return "1m"
+            case Interval.FIVE_MINUTES:
+                return "5m"
+            case Interval.FIFTEEN_MINUTES:
+                return "15m"
+            case Interval.ONE_HOUR:
+                return "1h"
+            case Interval.ONE_DAY:
+                return "1d"
+            case _:
+                raise Exception("Interval not supported")
