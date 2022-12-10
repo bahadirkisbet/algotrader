3ba5c54e0f8fb363bc6b9352b31a47327ef6cbdc
bahadirkisbet <bahadirkisbet@gmail.com>
2022-12-11 01:59:27 +0300

    [refactored & performance improvement]

--- data_provider/exchange_collection/exchange_library/binance/binance_base.py
+++ data_provider/exchange_collection/exchange_library/binance/binance_base.py
@@ -1,5 +1,6 @@
 from data_provider.exchange_collection.exchange_base import *
 from common_models.time_models import Interval
+import datetime
 
 
 class BinanceBase(ExchangeBase, ABC):
@@ -29,3 +30,18 @@ class BinanceBase(ExchangeBase, ABC):
                 return "1d"
             case _:
                 raise Exception("Interval not supported")
+
+    def _create_url_list_(self, endDate, interval, startDate, symbol):
+        url_list = []
+        current_date = startDate
+        while current_date <= endDate:
+            next_date = current_date + datetime.timedelta(minutes=interval.value)
+            url = self.api_url + self.api_endpoints["fetch_candle"].format(
+                symbol=symbol,
+                interval=self.interval_to_granularity(interval),
+                start=int(current_date.timestamp() * 1000),
+                end=int(next_date.timestamp() * 1000)
+            )
+            url_list.append([url])
+            current_date = next_date
+        return url_list
