487fbd3644901d7f8332b18dab61b1f3c9d5e5da
bahadirkisbet <bahadirkisbet@gmail.com>
2023-02-02 17:33:07 +0100

    [improvement] better style

--- common_models/data_models/candle.py
+++ common_models/data_models/candle.py
@@ -11,12 +11,12 @@ class Candle:
                  close: float,
                  volume: float,
                  trade_count: int):
+        self.timestamp = timestamp
         self.open = open
         self.high = high
         self.low = low
         self.close = close
         self.volume = volume
-        self.timestamp = timestamp
         self.trade_count = trade_count
 
     def __str__(self):
@@ -28,13 +28,6 @@ class Candle:
                f"{self.volume} - " \
                f"{self.trade_count}"
 
-    def get_readable(self):
-        return f"""
-            Timestamp: {self.timestamp}
-            Open: {self.open}
-            High: {self.high}
-            Low: {self.low}
-            Close: {self.close}
-            Volume: {self.volume}
-            Trade Count: {self.trade_count}
-        """
+    def get_json(self):
+        return vars(self)
+
