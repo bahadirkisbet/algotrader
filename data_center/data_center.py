1b59d3dc11838412280cd0036ae9bfe0660fd7ed
bahadirkisbet <bahadirkisbet@gmail.com>
2023-02-19 17:08:15 +0100

    [feature] added main loop

--- data_center/data_center.py
+++ data_center/data_center.py
@@ -1,28 +1,37 @@
+import configparser
 import datetime
 import logging
-from typing import List
+from typing import List, Dict
 
 from common_models.data_models.candle import Candle
 from data_provider.exchange_collection.exchange_base import ExchangeBase
 from startup import ServiceManager
 from utils.singleton_metaclass.singleton import Singleton
 from common_models.time_models import Interval
+from queue import Queue
+from multiprocessing import Lock
 
 
 class DataCenter(metaclass=Singleton):
     def __init__(self):
-        self.__buffer__: List[Candle] = []
-        self.exchange = ServiceManager.get_service("exchange")
-        self.logger = ServiceManager.get_service("logger")
-        self.config = ServiceManager.get_service("config")
+        self.__buffer__: Queue[Candle] = Queue()
+        self.exchange: ExchangeBase = ServiceManager.get_service("exchange")
+        self.logger: logging.Logger = ServiceManager.get_service("logger")
+        self.config: configparser.ConfigParser = ServiceManager.get_service("config")
+        self.symbols: Dict[str, List[Candle]] = {}
+        self.application_lock = Lock()
+
+        # register callbacks
+        self.exchange.register_candle_callback(self.push_candle)
 
     def start(self):
         symbols = self.exchange.fetch_product_list()
         self.logger.info(f"Total symbols: {len(symbols)}")
-        self.exchange.subscribe_to_websocket(symbols, Interval.FIVE_MINUTES)
+        self.exchange.subscribe_to_websocket(symbols, Interval.ONE_MINUTE)
+        self.run_forever()
 
     def push_candle(self, candle: Candle):
-        self.__buffer__.append(candle)
+        self.__buffer__.put(candle)
 
     def print_info(self, message, error_code):
         self.logger.info(message + " " + str(error_code))
@@ -36,3 +45,11 @@ class DataCenter(metaclass=Singleton):
 
         for symbol in symbols:
             candles = self.exchange.fetch_candle(symbol, start_date, end_date, interval)
+
+    def run_forever(self):
+        while True:
+            candle = self.__buffer__.get()
+            if candle.symbol not in self.symbols:
+                self.symbols[candle.symbol] = []
+            self.symbols[candle.symbol].append(candle)
+            self.logger.info(candle)
