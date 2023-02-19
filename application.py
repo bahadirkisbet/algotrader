1c22e4b45e75ab1f5b0ac290670238b1931dab3c
bahadirkisbet <bahadirkisbet@gmail.com>
2023-02-19 17:09:20 +0100

    [improvement]

--- application.py
+++ application.py
@@ -1,9 +1,18 @@
+from common_models.exchange_type import ExchangeType
+from data_center.data_center import DataCenter
+from data_provider.exchange_collection.exchange_factory import ExchangeFactory
 from startup import ServiceManager
 
 
 if __name__ == "__main__":
     ServiceManager.initialize_logger()
     ServiceManager.initialize_config()
-    ServiceManager.initialize_exchange_service()
+    config = ServiceManager.get_service("config")
+    exchange_name = config["EXCHANGE"]["exchange_code"]
+    exchange = ExchangeFactory.create(exchange_name, ExchangeType.SPOT)
+    ServiceManager.add_service("exchange", exchange)
+
+    dataCenter = DataCenter()
+    dataCenter.start()
 
     input()
