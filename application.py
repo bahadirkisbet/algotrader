bb59610f3cd28d8b22303808fb8c766df1591433
bahadirkisbet <bahadirkisbet@gmail.com>
2023-02-27 00:56:43 +0100

    [feature] some tests

--- application.py
+++ application.py
@@ -1,12 +1,13 @@
+import time
+
 from common_models.exchange_type import ExchangeType
 from data_center.data_center import DataCenter
 from data_provider.exchange_collection.exchange_factory import ExchangeFactory
-from startup import ServiceManager
+from startup import inject_services, ServiceManager
 
 
 if __name__ == "__main__":
-    ServiceManager.initialize_logger()
-    ServiceManager.initialize_config()
+    inject_services()
     config = ServiceManager.get_service("config")
     exchange_name = config["EXCHANGE"]["exchange_code"]
     exchange = ExchangeFactory.create(exchange_name, ExchangeType.SPOT)
@@ -14,5 +15,5 @@ if __name__ == "__main__":
 
     dataCenter = DataCenter()
     dataCenter.start()
-
-    input()
+    time.sleep(60)
+    dataCenter.close()
